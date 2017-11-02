b10aac2b5a2d5cb0df5ca7588914cb0e4c572a29
Jesse Rodriguez <Aertex@users.noreply.github.com>
2017-11-02 16:56:44 -0700

    actually organized holy shit

---
+++ teamproject/teamproject.srcs/sources_1/new/Deserializer.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/02/2017 04:54:46 PM
+// Design Name: 
+// Module Name: Deserializer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Deserializer(
+
+    );
+endmodule
---
+++ teamproject/teamproject.srcs/sources_1/new/TopModule.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/02/2017 04:55:45 PM
+// Design Name: 
+// Module Name: TopModule
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+//instantiate all modules here/act as main hub
+module TopModule(
+
+    );
+endmodule
