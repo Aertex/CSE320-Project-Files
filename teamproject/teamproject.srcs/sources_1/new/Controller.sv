613922acd7c88b988e34f2218829cfdd1f0b66c4
Jesse Rodriguez <Aertex@users.noreply.github.com>
2017-11-28 14:56:55 -0700

    tnjkbjk

--- teamproject/teamproject.srcs/sources_1/new/Controller.sv
+++ teamproject/teamproject.srcs/sources_1/new/Controller.sv
@@ -1,2 +1,18 @@
 //main driver, controlls everything
 //use created memory, datainA will be writing in, datainB will be reading out
+module Controller(
+input logic play,
+input logic record,
+input logic reset,
+input logic play_select,
+input logic record_select,
+
+input logic seconds2, //input from timer to let controller know 2 seconds have passed
+
+output logic [1:0]memoryselect_clip_1, //2rd bit = write enable, 1nd bit = enable for memory reading/writing
+output logic [1:0]memoryselect_clip_2 //2rd bit = write enable, 1nd bit = enable for memory reading/writing
+
+);
+
+
+endmodule
--- teamproject/teamproject.srcs/sources_1/new/TopModule.sv
+++ teamproject/teamproject.srcs/sources_1/new/TopModule.sv
@@ -1,26 +1,17 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/02/2017 04:55:45 PM
-// Design Name: 
-// Module Name: TopModule
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-//instantiate all modules here/act as main hub
+//just holds container, 
 module TopModule(
+input logic switch0,
+input logic switch1,
+input logic reset,
+input logic play,
+input logic record,
+input logic microphone,
+output logic audio_out,
+output logic a0,
+output logic a1,
+output logic [6:0]cathode
 
     );
 endmodule
