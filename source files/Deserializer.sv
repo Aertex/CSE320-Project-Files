4c0c8918537580443e304393e072f92bdfd4f38d
Jesse Rodriguez <Aertex@users.noreply.github.com>
2017-11-28 16:23:05 -0700

    gjabjf

---
+++ source files/Deserializer.sv
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+//reads input from microphone, stores it into memory
+//needs to read a line of 0/1 and creates a 16 bit word
+//outputs done signal every 16 clock cycles
+//needs to convert a 100mhz clock signal to 1mhz signal internally
+module Deserializer(
+
+    );
+endmodule
---
+++ source files/TopModule.sv
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+//just holds container, 
+module TopModule(
+input logic switch0,
+input logic switch1,
+input logic reset,
+input logic play,
+input logic record,
+input logic microphone,
+output logic audio_out,
+output logic a0,
+output logic a1,
+output logic [6:0]cathode
+
+    );
+
+
+
+
+endmodule
