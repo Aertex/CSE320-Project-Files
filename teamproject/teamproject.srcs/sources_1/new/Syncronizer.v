18b5c26eb2cbdf3ada99b8d6fb6663739cf052e7
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2017-11-28 14:33:35 -0700

    hj

--- teamproject/teamproject.srcs/sources_1/new/Syncronizer.v
+++
@@ -1,2 +0,0 @@
-//Intended for use to syncronize buttons and switches to feed inputs to the controller
-//probably put the inputs behind a flip flop or two
---
+++ teamproject/teamproject.srcs/sources_1/new/synchronizer.sv
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/28/2017 02:23:57 PM
+// Design Name: 
+// Module Name: synchronizer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module synchronizer(
+//buttons
+input clock
+input logic reset,
+input logic record, j
+input logic play,
+//switches
+input logic clipselectionwr,
+input logic clipselectionr
+
+always _ff(posedge clock)
+begin
+
+end
+
+
+    );
+endmodule
