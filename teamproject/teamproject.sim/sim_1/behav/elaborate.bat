df873f0399cd56b89059f753ef455c89354db724
Fatkitties <33295966+Fatkitties@users.noreply.github.com>
2017-11-28 16:02:15 -0700

    asfd

--- teamproject/teamproject.sim/sim_1/behav/elaborate.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set xv_path=C:\\Xilinx\\Vivado\\2017.2\\bin
-call %xv_path%/xelab  -wto d927fc97b4c94d61828d64ced361c9df -m64 --debug typical --relax --mt 2 -L xil_defaultlib -L unisims_ver -L unimacro_ver -L secureip -L xpm --snapshot timer_tb_behav xil_defaultlib.timer_tb xil_defaultlib.glbl -log elaborate.log
-if "%errorlevel%"=="0" goto SUCCESS
-if "%errorlevel%"=="1" goto END
-:END
-exit 1
-:SUCCESS
-exit 0
--- teamproject/teamproject.srcs/sim_1/new/timer_tb.sv
+++ teamproject/teamproject.srcs/sim_1/new/timer_tb.sv
@@ -20,13 +20,11 @@
 //////////////////////////////////////////////////////////////////////////////////
 module timer_tb();
 logic enable;
-logic reset;
 logic clock = 1'b0;
 logic oneMHzclk;
 
 timer time1(
 .enable(enable),
-.reset(reset),
 .clock(clock),
 .oneMHzclk(oneMHzclk)
 );
@@ -36,14 +34,8 @@ always #5 clock = ~clock;
 initial 
 begin
 enable = 0;
-reset = 1;
 
 #10 
-reset = 0;
-#15 
-reset = 1;
-#15 
-reset = 0;
 enable = 1;
 
 #100000000000000000000000
