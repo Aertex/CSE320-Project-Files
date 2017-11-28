9f2a88f2589235c6617ddbe5a935d951ded60697
Jesse Rodriguez <Aertex@users.noreply.github.com>
2017-11-28 16:22:23 -0700

    project reset lolololololo

--- teamproject/teamproject.srcs/sources_1/new/Address_Creator.sv
+++
@@ -1,3 +0,0 @@
-//intended for use in both deserializer/serializer to create address locations to read/write to memory
-
-//maybe it might be easier to implement this inside both serializer/deserializer, since this would run at 100 mhz, while both serializers run at 1mhz
--- teamproject/teamproject.srcs/sources_1/new/Serializer.sv
+++
@@ -1,9 +0,0 @@
-`timescale 1ns / 1ps
-//reads input from memory address via the address creator
-//reads 16 bit words and transforms it into 16 1-bit cycles
-//outputs done signal for 1 cycle every 16 cycles
-
-module Serializer(
-
-    );
-endmodule
