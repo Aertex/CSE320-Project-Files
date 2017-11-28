2106658d55645cda05c936707b9a1929679f08d9
Jesse Rodriguez <Aertex@users.noreply.github.com>
2017-11-28 14:13:27 -0700

    bgkjdsabfkadbhfkbd

--- teamproject/teamproject.srcs/sources_1/new/Segment_LED_Interface.sv
+++ teamproject/teamproject.srcs/sources_1/new/Segment_LED_Interface.sv
@@ -2,6 +2,19 @@
 //read from buttons/switch and display to led output, no need to connect to controller
 
 module Segment_LED_Interface(
-
+input logic switch0, //slect record clip, 1 or 2 J15 package pins
+input logic switch1, //select play clip, 1 or 2  L16
+output logic a0,
+output logic a1,
+output logic cathode[7:0]
     );
+    
+    always_comb
+    begin
+    if(switch0 == 1'b0)
+    begin
+    
+    end
+    
+    end
 endmodule
